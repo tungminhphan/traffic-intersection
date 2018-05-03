e8a17592804a4b3c760bd6a19ea0106aa6a8dade
Tung Phan <tung@caltech.edu>
2018-05-03 13:18:37 -0700

    input passable to dynamics

--- car.py
+++ car.py
@@ -1,3 +1,7 @@
+# Kinematic Car Class
+# Tung M. Phan
+# California Institute of Technology
+# May 3, 2018
 from numpy import cos, sin, tan
 import scipy.integrate as integrate
 import numpy as np
@@ -19,26 +23,41 @@ class KinematicCar:
     def __init__(self, init_state = [0, 0, 0, 0],
                  L = 3, # length of vehicle in meters
                  a_max = 9.81, # maximum acceleration of vehicle
-                 a_min = 9.81, # maximum deceleration of vehicle
+                 a_min = -9.81, # maximum deceleration of vehicle
                  nu_max = 0.5, # maximum steering input in radians
                  nu_min = -0.5): # minimum steering input in radians)
                      self.init_state = np.array(init_state, dtype="float")
                      self.params = (L, a_max, a_min, nu_max, nu_min)
-                     self.time_alive = 0
+                     self.alive_time = 0
                      self.state = self.init_state
-    def state_dot(self, state, u):
+    def state_dot(self,
+                  state,
+                  t,
+                  a,
+                  nu):
        """
        state_dot is a Function that defines the system dynamics
 
        """
        (L, a_max, a_min, nu_max, nu_min) = self.params
-       (a, nu) = u
+       dstate_dt = np.zeros(np.shape(state))
        dstate_dt[0] = saturation_filter(a, a_max, a_min)
        dstate_dt[1] = state[0] / L * tan(saturation_filter(nu, nu_max, nu_min))
        dstate_dt[2] = state[0] * cos(state[1])
        dstate_dt[3] = state[0] * sin(state[1])
        return dstate_dt
-   def next(self, dt):
-       self.state = integrate.odeint(self.state_dot, self.state, [0, dt])[1]
-       self.time_alive += dt
+
+    def next(self, inputs, dt):
+       # take only the real part of the solution
+       self.state = integrate.odeint(self.state_dot, self.state, t=(0, dt), args=inputs)[1]
+       self.alive_time += dt
+
+
+car = KinematicCar()
+dt = 0.1
+t = 0
+while t < 1:
+    car.next(inputs=(1,1),dt = dt)
+    print(car.state)
+    t += dt
 
