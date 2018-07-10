89c31bd92385c7e7b740e80d1167f0b9b814eaa7
Tung Phan <tung@caltech.edu>
2018-07-10 13:47:50 -0700

    update automata

--- traffic_intersection/contracts/pedestrian_behavior.py
+++ traffic_intersection/contracts/pedestrian_behavior.py
@@ -7,14 +7,19 @@ from graphviz import Digraph
 
 
 good_pedestrian = Digraph(filename='good_pedestrian.gv', comment='This is how a good pedestrian should behave')
-good_pedestrian.node_attr.update(color='green2', style='filled', fixedsize='true', width='1')
+good_pedestrian.node_attr.update(color='green', style='filled', fixedsize='true', width='1')
 
-good_pedestrian.attr('node', shape = 'doublecircle')
-good_pedestrian.node ('1', 'crossed')
 good_pedestrian.attr('node', shape = 'circle')
-good_pedestrian.node ('0', 'start', color='green3')
+good_pedestrian.node ('1', 'crossed')
+good_pedestrian.attr('node', shape = 'doublecircle')
+good_pedestrian.node ('2', 'exit')
+good_pedestrian.attr('node', shape = 'circle')
+good_pedestrian.node ('0', 'start', color='yellow')
 
-good_pedestrian.edge('0', '1', label='!(t_cross <= t_remaining) / !cross')
-good_pedestrian.edge('0', '0', label='⊤ / wait')
+good_pedestrian.edge('0', '1', label='t_cross <= t_remaining / !cross')
+good_pedestrian.edge('0', '0', label='⊤ / #wait')
+good_pedestrian.edge('1', '1', label='⊤ / #wait')
+good_pedestrian.edge('0', '2', label='⊤ / #walk')
+good_pedestrian.edge('1', '2', label='⊤ / #walk')
 good_pedestrian.view()
 
