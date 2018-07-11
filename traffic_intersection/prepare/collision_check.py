9a1456a4f0ceeafa50645aae7b12314d8672d627
Anhminh Nguyen <anhkhoanguyen05@debian.AK>
2018-07-10 22:30:25 -0700

    added header and function

--- traffic_intersection/prepare/collision_check.py
+++ traffic_intersection/prepare/collision_check.py
@@ -1,3 +1,9 @@
+# Collision Detection Cars and Pedestrians
+# Anhminh Nguyen
+# July 10, 2018
+
+
 # checks for pedestrian collision
+def collision_check():
         if (math.sqrt(((pedestrian_1.state[0] - pedestrian_2.state[0]) ** 2) + ((pedestrian_1.state[1] -  pedestrian_2.state[1]) ** 2)) < 76):
             print("There's a collision")
