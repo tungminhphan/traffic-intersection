d8c6687704dc299fd601301a83de7741783323cc
frosty <j4qfrost@gmail.com>
2018-07-30 02:08:21 -0700

    some refactoring

--- traffic_intersection/components/car.py
+++ traffic_intersection/components/car.py
@@ -3,7 +3,8 @@
 # California Institute of Technology
 # May 3, 2018
 
-import os, sys
+import os
+import sys
 sys.path.append("..")
 import scipy.io
 import numpy as np
@@ -20,7 +21,13 @@ from assumes.disturbance import get_disturbance
 dir_path = os.path.dirname(os.path.realpath(__file__))
 blue_car_fig = dir_path + "/imglib/cars/blue_car.png"
 gray_car_fig = dir_path + "/imglib/cars/gray_car.png"
+car_figs = {
+    "blue": blue_car_fig,
+    "gray": gray_car_fig
+}
 mat = scipy.io.loadmat(primitive_data)
+
+
 def saturation_filter(u, u_max, u_min):
     """ saturation_filter Helper Function
 
@@ -35,155 +42,165 @@ class KinematicCar:
     init_state is [vee, theta, x, y], where vee, theta, x, y are the velocity, orientation, and
     coordinates of the car respectively
     """
-    def __init__(self, init_state = [0, 0, 0, 0],
-                 L = 50, # length of vehicle
-                 a_max = 9.81, # maximum acceleration of vehicle
-                 a_min = -9.81, # maximum deceleration of vehicle
-                 nu_max = 0.5, # maximum steering input in radians/sec
-                 nu_min = -0.5, # minimum steering input in radians/sec)
-                 vee_max = 100, # maximum velocity
-                 is_honking = False, # the car is honking
-                 color = 'blue', # color of the car
-                 prim_queue = None, # queue of primitives, each item in the queue has the form (prim_id, prim_progress) where prim_id is the primitive ID and prim_progress is the progress of the primitive)
-                 fuel_level = float('inf')): # TODO: fuel level of the car - FUTURE FEATURE)
-                     if color != 'blue' and color != 'gray':
-                         raise Exception("Color must either be blue or gray!")
-                     self.params = (L, a_max, a_min, nu_max, nu_min, vee_max)
-                     self.alive_time = 0
-                     self.state = np.array(init_state, dtype='float')
-                     self.color = color
-                     self.extended_state = None # extended state required for Bastian's primitive computation
-                     self.is_honking = is_honking
-                     if prim_queue == None:
-                         self.prim_queue = Queue()
-                     else:
-                         self.prim_queue = prim_queue
-                     self.fuel_level = fuel_level
-                     if color == 'blue':
-                         self.fig = Image.open(blue_car_fig)
-                     else:
-                         self.fig = Image.open(gray_car_fig)
+
+    def __init__(self, init_state=[0, 0, 0, 0],
+                 L=50,  # length of vehicle
+                 a_max=9.81,  # maximum acceleration of vehicle
+                 a_min=-9.81,  # maximum deceleration of vehicle
+                 nu_max=0.5,  # maximum steering input in radians/sec
+                 nu_min=-0.5,  # minimum steering input in radians/sec)
+                 vee_max=100,  # maximum velocity
+                 is_honking=False,  # the car is honking
+                 color='blue',  # color of the car
+                 # queue of primitives, each item in the queue has the form (prim_id, prim_progress) where prim_id is the primitive ID and prim_progress is the progress of the primitive)
+                 prim_queue=None,
+                 fuel_level=float('inf')):  # TODO: fuel level of the car - FUTURE FEATURE)
+        if color != 'blue' and color != 'gray':
+            raise Exception("Color must either be blue or gray!")
+        self.color = color
+        self.fig = Image.open(car_figs[color])
+        self.params = (L, a_max, a_min, nu_max, nu_min, vee_max)
+        self.alive_time = 0
+        self.state = np.array(init_state, dtype='float')
+        # extended state required for Bastian's primitive computation
+        self.extended_state = None
+        self.is_honking = is_honking
+        self.prim_queue = Queue() if prim_queue is None else prim_queue
+        self.fuel_level = fuel_level
 
     def state_dot(self,
                   state,
                   t,
                   a,
                   nu):
-       """
-       state_dot is a function that defines the system dynamics
+        """
+        state_dot is a function that defines the system dynamics
 
-       Inputs
-       state: current state
-       t: current time
-       a: acceleration input
-       nu: steering input
+        Inputs
+        state: current state
+        t: current time
+        a: acceleration input
+        nu: steering input
 
-       """
-       (L, a_max, a_min, nu_max, nu_min, vee_max) = self.params
-       dstate_dt = np.zeros(np.shape(state))
-       dstate_dt[0] = saturation_filter(a, a_max, a_min)
-       # if already at maximum speed, can't no longer accelerate
-       if np.abs(state[1]) >= vee_max and np.sign(a) == np.sign(state[1]):
-           dstate_dt[1] = 0
-       else:
-           dstate_dt[1] = state[0] / L * tan(saturation_filter(nu, nu_max, nu_min))
-       dstate_dt[2] = state[0] * cos(state[1])
-       dstate_dt[3] = state[0] * sin(state[1])
-       return dstate_dt
+        """
+        (L, a_max, a_min, nu_max, nu_min, vee_max) = self.params
+        dstate_dt = np.zeros(np.shape(state))
+        dstate_dt[0] = saturation_filter(a, a_max, a_min)
+        # if already at maximum speed, can't no longer accelerate
+        if np.abs(state[1]) >= vee_max and np.sign(a) == np.sign(state[1]):
+            dstate_dt[1] = 0
+        else:
+            dstate_dt[1] = state[0] / L * \
+                tan(saturation_filter(nu, nu_max, nu_min))
+        dstate_dt[2] = state[0] * cos(state[1])
+        dstate_dt[3] = state[0] * sin(state[1])
+        return dstate_dt
 
     def toggle_honk(self):
         self.is_honking = not self.is_honking
 
     def next(self, inputs, dt):
-       """
-       next is a function that updates the current position of the car when inputs are applied for a duration of dt
-       Inputs:
-       inputs: acceleration and steering inputs
-       dt: integration time
+        """
+        next is a function that updates the current position of the car when inputs are applied for a duration of dt
+        Inputs:
+        inputs: acceleration and steering inputs
+        dt: integration time
 
-       Outputs:
-       None - the states of the car will get updated
-       """
-       a, nu = inputs
+        Outputs:
+        None - the states of the car will get updated
+        """
+        a, nu = inputs
 
-       # take only the real part of the solution
-       self.state = odeint(self.state_dot, self.state, t=(0, dt), args=(a, nu))[1]
-       # fuel decreases linearly with acceleration/deceleration
-       self.fuel_level -= np.abs(a) * dt
-       # update alive time
-       self.alive_time += dt
+        # take only the real part of the solution
+        self.state = odeint(self.state_dot, self.state,
+                            t=(0, dt), args=(a, nu))[1]
+        # fuel decreases linearly with acceleration/deceleration
+        self.fuel_level -= np.abs(a) * dt
+        # update alive time
+        self.alive_time += dt
 
-       # TODO: temporary fix to floating problem
-       if a == 0:
-           self.state[0] = np.sign(self.state[0]) * abs(self.state[0]) * dt * 0.05
+        # TODO: temporary fix to floating problem
+        if a == 0:
+            self.state[0] = np.sign(self.state[0]) * \
+                abs(self.state[0]) * dt * 0.05
 
     def extract_primitive(self):
-       #TODO: rewrite the comment below
-       """
-       This function updates the primitive queue and picks the next primitive to be applied. When there is no more primitive in the queue, it will
-       return False
+        # TODO: rewrite the comment below
+        """
+        This function updates the primitive queue and picks the next primitive to be applied. When there is no more primitive in the queue, it will
+        return False
 
-       """
-       while self.prim_queue.len() > 0:
-           if self.prim_queue.top()[1] < 1: # if the top primitive hasn't been exhausted
-               prim_id, prim_progress = self.prim_queue.top() # extract it
-               return prim_id, prim_progress
-           else:
-               self.prim_queue.pop() # pop it
-       return False
+        """
+        while self.prim_queue.len() > 0:
+            # if the top primitive hasn't been exhausted
+            if self.prim_queue.top()[1] < 1:
+                prim_id, prim_progress = self.prim_queue.top()  # extract it
+                return prim_id, prim_progress
+            else:
+                self.prim_queue.pop()  # pop it
+        return False
 
     def prim_next(self, dt):
-       """
-       updates with primitive, if no primitive available, update with next with zero inputs
-       Inputs:
-       dt: integration time
+        """
+        updates with primitive, if no primitive available, update with next with zero inputs
+        Inputs:
+        dt: integration time
 
-       Outputs:
-       None - the states of the car will get updated
-       """
-       #TODO: implement compatibility check with primitive to make sure that the params & dynamics match (essentially comparing prim_car and the
-       #kinematic model)
-       if self.extract_primitive() == False: # if there is no primitive to use
-           self.next((0, 0), dt)
-       else:
-           prim_id, prim_progress = self.extract_primitive()
-           # load primitive data TODO: make this portion of the code more automated
-           prim = mat['MA3'][prim_id,0] # the primitive corresponding to the primitive number
-           t_end = prim['t_end'][0,0][0,0] # extract duration of primitive
-           N = prim['K'][0,0].shape[0] # number of subintervals encoded in primitive
-           G_u = np.diag([175, 1.29]) # this diagonal matrix encodes the size of input set (a constraint)
-           nu = 2 # number of inputs
-           nx = 4 # number of states
+        Outputs:
+        None - the states of the car will get updated
+        """
+        # TODO: implement compatibility check with primitive to make sure that the params & dynamics match (essentially comparing prim_car and the
+        # kinematic model)
+        if self.extract_primitive() == False:  # if there is no primitive to use
+            self.next((0, 0), dt)
+        else:
+            prim_id, prim_progress = self.extract_primitive()
+            # load primitive data TODO: make this portion of the code more automated
+            # the primitive corresponding to the primitive number
+            prim = mat['MA3'][prim_id, 0]
+            t_end = prim['t_end'][0, 0][0, 0]  # extract duration of primitive
+            # number of subintervals encoded in primitive
+            N = prim['K'][0, 0].shape[0]
+            # this diagonal matrix encodes the size of input set (a constraint)
+            G_u = np.diag([175, 1.29])
+            nu = 2  # number of inputs
+            nx = 4  # number of states
 
-           if prim_progress == 0: # compute initial extended state
-               x1 = self.state.reshape((-1,1))
-               x2 = prim['x0'][0,0]
-               x3 = x1 - prim['x0'][0,0]
-               x4 = np.matmul(np.linalg.inv(np.diag([4, 0.02, 4, 4])), (x1-prim['x0'][0,0]))
-               self.extended_state = (np.vstack((x1,x2,x3,x4)))[:,0] # initial state, consisting of actual state and virtual states for the controller
-           k = int(prim_progress * N) # calculate primitive waypoint
+            if prim_progress == 0:  # compute initial extended state
+                x1 = self.state.reshape((-1, 1))
+                x2 = prim['x0'][0, 0]
+                x3 = x1 - prim['x0'][0, 0]
+                x4 = np.matmul(np.linalg.inv(
+                    np.diag([4, 0.02, 4, 4])), (x1-prim['x0'][0, 0]))
+                # initial state, consisting of actual state and virtual states for the controller
+                self.extended_state = (np.vstack((x1, x2, x3, x4)))[:, 0]
+            k = int(prim_progress * N)  # calculate primitive waypoint
 
-           dist = get_disturbance()
-           q1 = prim['K'][0,0][k,0].reshape((-1, 1), order='F')
-           q2 = 0.5 * (prim['x_ref'][0,0][:,k+1] + prim['x_ref'][0,0][:,k]).reshape(-1,1)
-           q3 = prim['u_ref'][0,0][:,k].reshape(-1,1)
-           q4 = prim['u_ref'][0,0][:,k].reshape(-1,1)
-           q5 = np.matmul(G_u, prim['alpha'][0,0][k*nu:(k+1)*nu]).reshape((-1,1), order='F')
-           q = np.vstack((q1,q2,q3,q4,q5)) # parameters for the controller
-           self.extended_state = odeint(func = prim_state_dot, y0 = self.extended_state, t= [0, dt], args=(dist, q))[-1, :]
-           self.state = self.extended_state[0:4]
-           # update alive time
-           self.alive_time += dt
-           # update progress
-           prim_progress = prim_progress + dt / t_end
-           self.prim_queue.replace_top((prim_id, prim_progress))
+            dist = get_disturbance()
+            q1 = prim['K'][0, 0][k, 0].reshape((-1, 1), order='F')
+            q2 = 0.5 * (prim['x_ref'][0, 0][:, k+1] +
+                        prim['x_ref'][0, 0][:, k]).reshape(-1, 1)
+            q3 = prim['u_ref'][0, 0][:, k].reshape(-1, 1)
+            q4 = prim['u_ref'][0, 0][:, k].reshape(-1, 1)
+            q5 = np.matmul(G_u, prim['alpha'][0, 0]
+                           [k*nu:(k+1)*nu]).reshape((-1, 1), order='F')
+            # parameters for the controller
+            q = np.vstack((q1, q2, q3, q4, q5))
+            self.extended_state = odeint(func=prim_state_dot, y0=self.extended_state, t=[
+                                         0, dt], args=(dist, q))[-1, :]
+            self.state = self.extended_state[0:4]
+            # update alive time
+            self.alive_time += dt
+            # update progress
+            prim_progress = prim_progress + dt / t_end
+            self.prim_queue.replace_top((prim_id, prim_progress))
 
 # TESTING
 #prim_id = 0
-#prim = mat['MA3'][prim_id,0] # the primitive corresponding to the primitive number
-#x0 = np.array(prim['x0'][0,0]) + np.matmul(np.diag([4, 0.02, 4, 4]),2*np.random.rand(4,1)-np.ones((4,1))) # random state in initial set TODO: incorporate self.state
-#print(x0)
+# prim = mat['MA3'][prim_id,0] # the primitive corresponding to the primitive number
+# x0 = np.array(prim['x0'][0,0]) + np.matmul(np.diag([4, 0.02, 4, 4]),2*np.random.rand(4,1)-np.ones((4,1))) # random state in initial set TODO: incorporate self.state
+# print(x0)
 #my_car = KinematicCar(init_state = np.reshape(x0, (-1, 1)))
 #progress = 0
-#while my_car.prim_progress <= 1:
+# while my_car.prim_progress <= 1:
 #    my_car.prim_next(prim_id = 0, dt = 0.1)
--- traffic_intersection/contracts/alt_aut.py
+++ traffic_intersection/contracts/alt_aut.py
@@ -10,28 +10,29 @@ import itertools
 import numpy as np
 
 # Inequality for guard. Left-hand side is variable name, and is bounded on both sides (if one-sided inequality, has +/- infty on other side.)
+
+
 class Inequality:
-    def __init__(self, var, lower = -np.inf, upper = np.inf):
-        self.var = var # variable name
-        self.lwrbnd = lower # the lower bound
-        self.uprbnd = upper # the upper bound
+    def __init__(self, var, lower=-np.inf, upper=np.inf):
+        self.var = var  # variable name
+        self.lwrbnd = lower  # the lower bound
+        self.uprbnd = upper  # the upper bound
 
     def show(self):
         if self.lwrbnd > self.uprbnd:
-            txt = False
+            return False
         elif self.lwrbnd == -np.inf and self.uprbnd == np.inf:
-            txt = True
+            return True
         elif self.lwrbnd == self.uprbnd:
-            txt = self.var + " = " + str(self.lwrbnd)
+            return self.var + " = " + str(self.lwrbnd)
         elif self.lwrbnd == -np.inf and self.uprbnd != np.inf:
-            txt = self.var + ' ≤ ' + str(self.uprbnd)
+            return self.var + ' ≤ ' + str(self.uprbnd)
         elif self.lwrbnd != -np.inf and self.uprbnd == np.inf:
-            txt = str(self.lwrbnd) + ' ≤ ' + self.var
+            return str(self.lwrbnd) + ' ≤ ' + self.var
         elif self.lwrbnd != -np.inf and self.uprbnd == np.inf:
-            txt = str(self.lwrbnd) + ' ≤ ' + self.var
+            return str(self.lwrbnd) + ' ≤ ' + self.var
         else:
-            txt = str(self.lwrbnd) + ' ≤ ' + self.var + ' ≤ ' + str(self.uprbnd)
-        return txt
+            return str(self.lwrbnd) + ' ≤ ' + self.var + ' ≤ ' + str(self.uprbnd)
 
 
 # returns a set of inequalities whose conjunction has the same truth value as the conjunction of two sets of inequalities
@@ -46,11 +47,10 @@ def conjunct(ineq_dict1, ineq_dict2):
     for key in shared_keys:
         ineq1 = ineq_dict1[key]
         ineq2 = ineq_dict2[key]
-        new_ineq = Inequality(ineq1.var, min(ineq1.lwrbnd, ineq2.lwrbnd), max(ineq1.uprbnd,ineq2.uprbnd)) # take the conjunction of the two inqualities
+        new_ineq = Inequality(ineq1.var, min(ineq1.lwrbnd, ineq2.lwrbnd), max(
+            ineq1.uprbnd, ineq2.uprbnd))  # take the conjunction of the two inqualities
         if new_ineq.show() == False:
             return False
-        elif new_ineq.show() != True:
-            new_dict[key] = new_ineq
 
     for key in different_keys:
         if key in ineq_dict1.keys():
@@ -59,115 +59,132 @@ def conjunct(ineq_dict1, ineq_dict2):
             new_dict[key] = ineq_dict2[key]
     return new_dict
 
+
 def dictionarize(ineq):
     ineq_dict = dict()
     ineq_dict[ineq.var] = ineq
     return ineq_dict
 
-def pretty_print(ineq_dict): # print contents of a dictionary of inequalities
+
+def pretty_print(ineq_dict):  # print contents of a dictionary of inequalities
     keys = sorted(ineq_dict.keys())
     for key in keys:
         print(ineq_dict[key].show())
 
 # TODO: fix comments returns a set of inequalities that has the same truth value as the disjunction of two sets of inequalities# returns an inequality that has the same truth value as disjunction of two inequalities
+
+
 def disjunct(ineq1, ineq2):
     if ineq1.var == ineq2.var:
         return Inequality(guard1.var, min(ineq1.lwrbnd, ineq2.lwrbnd), max(ineq1.uprbnd, ineq2.uprbnd))
     else:
-        return False # ? don't need to ever use this in this case
+        return False  # ? don't need to ever use this in this case
 
 # returns an inequality that has the same truth value as the conjunction of two inequalities (or if
 # the two inequalites correspond to different variables, a dictionary
 
-eq1 = dictionarize(Inequality(lower = 3, var = 'x', upper = 3))
-eq2 = dictionarize(Inequality(lower = -9, var = 'z', upper = 5))
-eq3 = dictionarize(Inequality(lower = 2, var = 'x', upper = 7))
-eq4 = dictionarize(Inequality(lower = -3, var = 'z', upper = 10))
-eq =  conjunct(conjunct(eq1,eq2), conjunct(eq3,eq4))
+
+eq1 = dictionarize(Inequality(lower=3, var='x', upper=3))
+eq2 = dictionarize(Inequality(lower=-9, var='z', upper=5))
+eq3 = dictionarize(Inequality(lower=2, var='x', upper=7))
+eq4 = dictionarize(Inequality(lower=-3, var='z', upper=10))
+eq = conjunct(conjunct(eq1, eq2), conjunct(eq3, eq4))
 pretty_print(eq)
 
 # Transition class. Has guard and optional input/output/internal action
+
+
 class Transition:
-    def __init__(self, ineq = True, inp = None, out = None, inter = None, s1 = None, s2 = None):
-        self.guard = ineq # guard is a set of inequalities joined by conjunction (?)
-        self.input = inp # set of inputs
+    def __init__(self, ineq=True, inp=None, out=None, inter=None, s1=None, s2=None):
+        # guard is a set of inequalities joined by conjunction (?)
+        self.guard = ineq
+        self.input = inp  # set of inputs
         self.output = out
-        self.internal = inter # internal action?
-        self.state1 = s1 # set
-        self.state2 = s2 # transition from s1 to s2
+        self.internal = inter  # internal action?
+        self.state1 = s1  # set
+        self.state2 = s2  # transition from s1 to s2
+
 
 def compose_transitions(tr1, tr2):
     guard = conjunct(tr1, tr2)
     inp = tr1.input.union(tr2.input)
-    inter = (tr1.input.intersection(tr2.output)).union(tr1.output.intersection(tr1.input))
+    inter = (tr1.input.intersection(tr2.output)).union(
+        tr1.output.intersection(tr1.input))
     out = tr1.output.intersection(tr2.output)
     return Transition(guard, inp, out, inter, (tr1.state1, tr2.state1), (tr1.state2, tr2.state2))
 
 
 class ComponentAutomaton:
     def __init__(self):
-        self.alphabet = {} # the alphabet for the component
-        self.transitions_dict = {} # dictionary of all transitions
+        self.alphabet = {}  # the alphabet for the component
+        self.transitions_dict = {}  # dictionary of all transitions
         self.startState = None
         self.endStates = {}
 
-    def add_state(self, name, transitions, end_state = 0):
+    def add_state(self, name, transitions, end_state=0):
         if end_state:
             self.endStates.append(name.upper())
-        self.transitions_dict[name.upper()] = map(lambda x: x.upper(), transitions)
+        self.transitions_dict[name.upper()] = map(
+            lambda x: x.upper(), transitions)
 
     def set_start_state(self, name):
         self.startState = name.upper()
 
-    def simulate(self, is_printing = False):
+    def simulate(self, is_printing=False):
         state = self.startState
         while state not in self.endStates:
             if is_printing:
                 print(state + "  taking transitions, output, input")
             else:
                 print("taking transitions, output, input")
-            state, = sample(self.transitions_dict[state],1) # sample from set of all transistions uniformly at random
+            # sample from set of all transistions uniformly at random
+            state, = sample(self.transitions_dict[state], 1)
         print("simulation has terminated or deadlocked!")
 
     def compose_components(component_1, component_2):
         new_component = ComponentAutomaton()
         newstart = (component_1.startState, component_2.startState)
         newtransitions = []
-        newends = itertools.product(component_1.endStates, component_2.endStates)
+        newends = itertools.product(
+            component_1.endStates, component_2.endStates)
         dict1 = component_1.transitions_dict
         dict2 = component_2.transitions_dict
         for key1 in dict1:
             for key2 in dict2:
-                newtransitions.append(compose_transitions(dict1[key1], dict2[key2]))
+                newtransitions.append(
+                    compose_transitions(dict1[key1], dict2[key2]))
         return newtransitions
+
 
 class ContractAutomaton:
     def __init__(self):
-        self.alphabet = {} # the alphabet for the contract
-        self.transitions_dict = {} # dictionary of all transitions
+        self.alphabet = {}  # the alphabet for the contract
+        self.transitions_dict = {}  # dictionary of all transitions
         self.startState = None
         self.endStates = {}
 
-    def add_state(self, name, transitions, end_state = 0):
+    def add_state(self, name, transitions, end_state=0):
         if end_state:
             self.endStates.append(name.upper())
-        self.transitions_dict[name.upper()] = map(lambda x: x.upper(), transitions)
+        self.transitions_dict[name.upper()] = map(
+            lambda x: x.upper(), transitions)
 
     def set_start_state(self, name):
         self.startState = name.upper()
 
-    def simulate(self, is_printing = False):
+    def simulate(self, is_printing=False):
         state = self.startState
         while state not in self.endStates:
             if is_printing:
                 print(state + "  taking transitions, output, input")
             else:
                 print("taking transitions, output, input")
-            state, = sample(self.transitions_dict[state],1) # sample from set of all transistions uniformly at random
+            # sample from set of all transistions uniformly at random
+            state, = sample(self.transitions_dict[state], 1)
         print("simulation has terminated or deadlocked!")
 
 
-# not 
+# not
 # def merge_transition_dicts(td_1, td_2):
 #     td_merged = td_1.copy()
 #     for key in td_2:
@@ -187,14 +204,7 @@ class ContractAutomaton:
 def refines_contracts(unrefined_c, refined_c):
     if unrefined_c.startState != refined_c.startState:
         return False
-    elif unrefined_c.startState != refined_c.startState:
-        return False
-    else:
-        for key in unrefined_c.transitions_dict:
-            if (key not in refined_c[key]):
-                return False
-            elif not refined_c[key].issubset(unrefined_c[key]):
-                return False
-            else:
-                continue
-        return True
+    for key in unrefined_c.transitions_dict or not refined_c[key].issubset(unrefined_c[key]):
+        if (key not in refined_c[key]):
+            return False
+    return True
