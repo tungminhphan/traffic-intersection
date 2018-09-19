9a32287b8c78fc381695b131e2a6576c0ec9de44
Tung Phan <tung@caltech.edu>
2018-09-18 19:17:19 -0700

    add sched and car aut

--- traffic_intersection/contracts/intersection_automaton.py
+++ traffic_intersection/contracts/intersection_automaton.py
@@ -19,7 +19,7 @@ h_trans_must[('g_h', 'y_h')] = {('True', 'r_v', '?')}
 # h_trans_must[('g_h', 'fail')] = {('True', 'g_v', '?'), ('True', 'y_v', '?')}
 h_trans_must[('y_h', 'r_h')] = {('h_timer == 5', 'r_h', '!')}
 h = construct_contract_automaton(state_set = h_traffic_states, starts = starts_h, musttrans = h_trans_must, maytrans = h_trans_may, input_alphabet = h_input_alph, output_alphabet = h_output_alph)
-	
+
 v_traffic_states = {'r_v', 'g_v', 'y_v', 'v_walk', 'both_r_v'}
 v_trans_must = {}
 v_trans_may = {}
@@ -75,22 +75,4 @@ v.convert_to_digraph().render('v', view = True)
 # road_auto = construct_contract_automaton(state_set = road_states, starts = starts_road, musttrans = road_trans_must, maytrans = road_trans_may)
 # road_auto.convert_to_digraph().render('road', view = True)
 
-car_states = {'0', '1', '2', '3'}
-car_must = {}
-car_may = {}
-car_start = {'0'}
-car_input_alph = {'rejected', 'accepted', 'primitives'}
-car_output_alph = {'request'}
-car_must[('0', '1')] = {('True', 'request', '!')}
-car_must[('1', '2')] = {('True', 'accepted', '?')}
-car_must[('1', '0')] = {('True', 'rejected', '!')}
-car_must[('2', '3')] = {('True', 'primitives', '?')}
-car_may[('3', '3')] = {('True', 'primitives', '?')}
-car_may[('3', '0')] = {('not_done', 'request', '!')}
-car_auto = construct_contract_automaton(state_set = car_states, starts = car_start, musttrans = car_must, maytrans = car_may, input_alphabet = car_input_alph, output_alphabet = car_output_alph)
-car_auto.convert_to_digraph().render('car', view = True)
-
-traff = compose_contract(h, v)
-composed = compose_contract(traff, p)
-composed.convert_to_digraph().render('total', view = True)
 
--- traffic_intersection/contracts/scheduler_automaton.py
+++ traffic_intersection/contracts/scheduler_automaton.py
@@ -1,20 +1,36 @@
 from contract_automaton import *
 
-scheduler_states = {'0', '1', '2'}
+car_states = {'0', '1', '2', '3'}
+car_must = {}
+car_may = {}
+car_start = {'0'}
+car_input_alph = {'reject', 'accept', 'primitives'}
+car_output_alph = {'request'}
+car_must[('0', '1')] = {('True', 'request', '!')}
+car_must[('1', '2')] = {('True', 'accept', '?')}
+car_must[('1', '0')] = {('True', 'reject', '?')}
+car_must[('2', '3')] = {('True', 'primitives', '?')}
+car_may[('3', '3')] = {('True', 'primitives', '?')}
+car_may[('3', '1')] = {('not_done', 'request', '!')}
+car_auto = construct_contract_automaton(state_set = car_states, starts = car_start, musttrans = car_must, maytrans = car_may, input_alphabet = car_input_alph, output_alphabet = car_output_alph)
+car_auto.convert_to_digraph().render('car', view = True)
+
+scheduler_states = {'0', '1', '2', '3'}
 trans_must = {}
 trans_may = {}
 starts_h = {'0'}
 input_alph = {'request'}
 output_alph = {'reject', 'accept', 'primitives'}
-internal_alph = {'reject', 'accept', 'primitives'}
+internal_alph = {'processing'}
 
-trans_may[('1', '0')] = {('True', 'reject', '!')}
-trans_must[('2', '0')] = {('True', 'primitives', '!')}
-trans_must[('1', '2')] = {('True', 'accept', '!')}
-trans_must[('0', '0')] = {('True', 'request', '?')}
-trans_must[('0', '1')] = {('len(request_queue)>0', 'processing', '#')}
+trans_may[('2', '0')] = {('True', 'reject', '!')}
+#trans_must[('1', '1')] = {('True', 'request', '?')}
+trans_must[('3', '0')] = {('True', 'primitives', '!')}
+trans_must[('2', '3')] = {('True', 'accept', '!')}
+trans_must[('0', '1')] = {('True', 'request', '?')}
+trans_must[('1', '2')] = {('len(request_queue)>0', 'processing', '#')}
 scheduler = construct_contract_automaton(state_set = scheduler_states, starts = starts_h, musttrans = trans_must, maytrans = trans_may, input_alphabet = input_alph, output_alphabet = output_alph, internal_alphabet = internal_alph)
-
-
 scheduler.convert_to_digraph().render('scheduler', view = True)
 
+sched_car = compose_contract(scheduler, car_auto)
+sched_car.convert_to_digraph().render('sched_car', view = True)
